0c03118037ae0600c1677917a0e483f01043077f
cryp70m4n <cryp70m4n@gmail.com>
2023-04-08 13:07:16 +0000

    Final

---
+++ suckless/dwm/themes/charon.h
@@ -0,0 +1,15 @@
+/* tagging */
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+/* fonts & size */
+static const char *fonts[]          = { "Iosevka:size=12" };
+static const char dmenufont[]       = "Iosevka:size=12";
+
+
+/* colors */
+static const char col_gray1[] = "#000000"; // deselected bg
+static const char col_gray2[] = "#393c32"; // deselected border
+static const char col_gray3[] = "#393c32"; // deselected text
+static const char col_gray4[] = "#c3703f"; // selected text
+static const char col_gray5[] = "#c3703f"; // selected border
+static const char col_cyan[] = "#000000"; // selected bg
